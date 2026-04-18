f3cd8e2f53722cd1eb382b71818a5a23d1c6bb86
zzwwff <zhouwufan@163.com>
2026-04-18 14:40:56 +0800

    Add logging for route registration in claw API

--- apis/claw/routes.go
+++ apis/claw/routes.go
@@ -3,10 +3,12 @@ package claw
 import (
     "github.com/gofiber/contrib/websocket"
     "github.com/gofiber/fiber/v2"
+    "github.com/rs/zerolog/log"
 )
 
 // RegisterRoutes 注册OpenClaw相关路由,尤其包含WebSocket端点
 func RegisterRoutes(app fiber.Router) {
+    log.Info().Msg("registering claw routes")
     // WebSocket 端点: /api/claw/ws
     app.Use("/claw/ws", func(c *fiber.Ctx) error {
         // 检查是否是WebSocket升级请求
@@ -18,5 +20,4 @@ func RegisterRoutes(app fiber.Router) {
 
     app.Get("/claw/ws", websocket.New(HandleWebSocket))
 	app.Post("/claw/test", clawtest)
-
 }
